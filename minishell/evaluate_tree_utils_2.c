e8d4020efbc920effde989a5dd808ff3b86f8f7c
ychng <ychng@student.42kl.edu.my>
2024-04-02 00:43:46 +0800

    fix norm in evaluate_tree_utils_2.c

--- minishell/evaluate_tree_utils_2.c
+++ minishell/evaluate_tree_utils_2.c
@@ -6,20 +6,16 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 04:51:00 by ychng             #+#    #+#             */
-/*   Updated: 2024/04/01 23:50:55 by ychng            ###   ########.fr       */
+/*   Updated: 2024/04/02 00:42:41 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void	handle_pipecmd(char ***envp, int pipefd[], int prev_pipefd[], \
-					t_subtokenlist **currcmd)
+int	get_redirfd(char ***envp, int *infilefd, int *outfilefd, \
+			t_subtokenlist *currcmd)
 {
 	t_subtokenlist	*redirlist;
-	int				infilefd;
-	int				outfilefd;
-	int				origstdin;
-	int				origstdout;
 
 	redirlist = extract_redirection(currcmd);
 	infilefd = get_infilefd(redirlist);
@@ -27,89 +23,91 @@ void	handle_pipecmd(char ***envp, int pipefd[], int prev_pipefd[], \
 	{
 		update_exit_status(*envp, 1);
 		free_subtokenlist(redirlist);
-		return ;
+		return (-1);
 	}
 	outfilefd = get_outfilefd(redirlist);
 	if (outfilefd == -1)
 	{
 		update_exit_status(*envp, 1);
 		free_subtokenlist(redirlist);
-		return ;
+		return (-1);
 	}
 	free_subtokenlist(redirlist);
+	return (0);
+}
+
+void	manage_piperedir_child(int pipefd[], int prev_pipefd[], \
+							int infilefd, int outfilefd)
+{
+	if (prev_pipefd[0] != 0)
+	{
+		close(prev_pipefd[1]);
+		dup2(prev_pipefd[0], STDIN_FILENO);
+		close(prev_pipefd[0]);
+	}
+	if (infilefd != 0)
+	{
+		dup2(infilefd, STDIN_FILENO);
+		close(infilefd);
+	}
+	close(pipefd[0]);
+	if (outfilefd != 0)
+	{
+		dup2(outfilefd, STDOUT_FILENO);
+		close(outfilefd);
+	}
+	else
+		dup2(pipefd[1], STDOUT_FILENO);
+	close(pipefd[1]);
+}
+
+void	manage_piperedir_parent(int pipefd[], int prev_pipefd[])
+{
+	if (prev_pipefd[0] != 0)
+	{
+		close(prev_pipefd[0]);
+		close(prev_pipefd[1]);
+	}
+	prev_pipefd[0] = pipefd[0];
+	prev_pipefd[1] = pipefd[1];
+}
+
+void	restore_originalfd(int origstdin, int origstdout)
+{
+	dup2(origstdin, STDIN_FILENO);
+	close(origstdin);
+	dup2(origstdout, STDOUT_FILENO);
+	close(origstdout);	
+}
+
+void	handle_pipecmd(char ***envp, int pipefd[], int prev_pipefd[], \
+					t_subtokenlist **currcmd)
+{
+	int				infilefd;
+	int				outfilefd;
+	int				origstdin;
+	int				origstdout;
+
+	if (get_redirfd(envp, &infilefd, &outfilefd, currcmd) == -1)
+		return ;
 	origstdin = dup(STDIN_FILENO);
 	origstdout = dup(STDOUT_FILENO);
 	if (create_fork() == 0)
 	{
-		if (prev_pipefd[0] != 0)
-		{
-			close(prev_pipefd[1]);
-			dup2(prev_pipefd[0], STDIN_FILENO);
-			close(prev_pipefd[0]);
-		}
-		if (infilefd != 0)
-		{
-			dup2(infilefd, STDIN_FILENO);
-			close(infilefd);
-		}
-		close(pipefd[0]);
-		if (outfilefd != 0)
-		{
-			dup2(outfilefd, STDOUT_FILENO);
-			close(outfilefd);
-		}
-		else
-			dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[1]);
+		manage_piperedir_child(pipefd, prev_pipefd, infilefd, outfilefd);
 		exit(run_cmd(envp, *currcmd));
 	}
 	else
-	{
-		if (prev_pipefd[0] != 0)
-		{
-			close(prev_pipefd[0]);
-			close(prev_pipefd[1]);
-		}
-		prev_pipefd[0] = pipefd[0];
-		prev_pipefd[1] = pipefd[1];
-	}
+		manage_piperedir_parent(pipefd, prev_pipefd);
 	if (infilefd != 0)
 		close(infilefd);
 	if (outfilefd != 0)
 		close(outfilefd);
-	dup2(origstdin, STDIN_FILENO);
-	close(origstdin);
-	dup2(origstdout, STDOUT_FILENO);
-	close(origstdout);
+	restore_originalfd(origstdin, origstdout);
 }
 
-void	handle_lastcmd(char ***envp, int prev_pipefd[], \
-					t_subtokenlist **currcmd)
+void	manage_lastcmdredir(int infilefd, int outfilefd)
 {
-	t_subtokenlist	*redirlist;
-	int				infilefd;
-	int				outfilefd;
-	int				origstdin;
-	int				origstdout;
-
-	redirlist = extract_redirection(currcmd);
-	infilefd = get_infilefd(redirlist);
-	if (infilefd == -1)
-	{
-		update_exit_status(*envp, 1);
-		free_subtokenlist(redirlist);
-		return ;
-	}
-	outfilefd = get_outfilefd(redirlist);
-	if (outfilefd == -1)
-	{
-		update_exit_status(*envp, 1);
-		free_subtokenlist(redirlist);
-		return ;
-	}
-	free_subtokenlist(redirlist);
-	origstdin = dup(STDIN_FILENO);
-	origstdout = dup(STDOUT_FILENO);
 	if (infilefd != 0)
 	{
 		dup2(infilefd, STDIN_FILENO);
@@ -120,32 +118,52 @@ void	handle_lastcmd(char ***envp, int prev_pipefd[], \
 		dup2(outfilefd, STDOUT_FILENO);
 		close(outfilefd);
 	}
+}
+
+void	handle_lastcmd_child(int prev_pipefd[], int infilefd)
+{
+	if (prev_pipefd[0] != 0)
+	{
+		close(prev_pipefd[1]);
+		if (infilefd == 0)
+			dup2(prev_pipefd[0], STDIN_FILENO);
+		close(prev_pipefd[0]);
+	}	
+}
+
+void	handle_lastcmd_parent(int prev_pipefd[])
+{
+	if (prev_pipefd[0] != 0)
+	{
+		close(prev_pipefd[0]);
+		close(prev_pipefd[1]);
+	}
+}
+
+void	handle_lastcmd(char ***envp, int prev_pipefd[], \
+					t_subtokenlist **currcmd)
+{
+	int				infilefd;
+	int				outfilefd;
+	int				origstdin;
+	int				origstdout;
+
+	if (get_redirfd(envp, &infilefd, &outfilefd, currcmd) == -1)
+		return ;
+	origstdin = dup(STDIN_FILENO);
+	origstdout = dup(STDOUT_FILENO);
+	manage_lastcmdredir(infilefd, outfilefd);
 	if (prev_pipefd[0] == 0 && is_builtins(*currcmd))
 		update_exit_status(*envp, run_cmd(envp, *currcmd));
 	else
 	{
 		if (create_fork() == 0)
 		{
-			if (prev_pipefd[0] != 0)
-			{
-				close(prev_pipefd[1]);
-				if (infilefd == 0)
-					dup2(prev_pipefd[0], STDIN_FILENO);
-				close(prev_pipefd[0]);
-			}
+			handle_lastcmd_child(prev_pipefd, infilefd);
 			exit(run_cmd(envp, *currcmd));
 		}
 		else
-		{
-			if (prev_pipefd[0] != 0)
-			{
-				close(prev_pipefd[0]);
-				close(prev_pipefd[1]);
-			}
-		}
+			handle_lastcmd_parent(prev_pipefd);
 	}
-	dup2(origstdin, STDIN_FILENO);
-	close(origstdin);
-	dup2(origstdout, STDOUT_FILENO);
-	close(origstdout);
+	restore_originalfd(origstdin, origstdout);
 }
