6da5dd0e34780727f9bf8d03a4f6671f26e03a8b
ychng <ychng@student.42kl.edu.my>
2024-04-06 01:07:23 +0800

    fix norm for build_tree.c

--- minishell/build_tree.c
+++ minishell/build_tree.c
@@ -6,17 +6,63 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 19:25:04 by ychng             #+#    #+#             */
-/*   Updated: 2024/03/30 01:46:41 by ychng            ###   ########.fr       */
+/*   Updated: 2024/04/06 01:05:26 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
+static void	handle_leftbracket(t_treenode **cmdlist, \
+									t_tokenlist **tokenlist)
+{
+	t_treenode	*treenode;
+
+	free_tokennode(pop_tokenlist_head(*tokenlist));
+	treenode = build_tree(*tokenlist);
+	push_(treenode, cmdlist);
+}
+
+static void	handle_logicalop(t_treenode **cmdlist, t_treenode **opslist, \
+							t_tokenlist **tokenlist)
+{
+	t_treenode	*treenode;
+
+	if (*opslist != NULL)
+	{
+		treenode = pop_treenode_from(opslist);
+		treenode->right = pop_treenode_from(cmdlist);
+		treenode->left = pop_treenode_from(cmdlist);
+		push_(treenode, cmdlist);
+	}
+	treenode = new_treenode(pop_tokenlist_head(*tokenlist));
+	push_(treenode, opslist);
+}
+
+static void	handle_default(t_treenode **cmdlist, t_tokenlist **tokenlist)
+{
+	t_treenode	*treenode;
+
+	treenode = new_treenode(pop_tokenlist_head(*tokenlist));
+	push_(treenode, cmdlist);
+}
+
+static void	empty_opslist(t_treenode **cmdlist, t_treenode **opslist)
+{
+	t_treenode	*treenode;
+
+	if (*opslist != NULL)
+	{
+		treenode = pop_treenode_from(opslist);
+		treenode->right = pop_treenode_from(cmdlist);
+		treenode->left = pop_treenode_from(cmdlist);
+		push_(treenode, cmdlist);
+	}
+}
+
 t_treenode	*build_tree(t_tokenlist *tokenlist)
 {
 	t_treenode	*cmdlist;
 	t_treenode	*opslist;
-	t_treenode	*treenode;
 	char		*firsubtok;
 
 	cmdlist = NULL;
@@ -25,40 +71,17 @@ t_treenode	*build_tree(t_tokenlist *tokenlist)
 	{
 		firsubtok = currsubtok_in(tokenlist);
 		if (is_leftbracket(firsubtok[0]))
-		{
-			free_tokennode(pop_tokenlist_head(tokenlist));
-			treenode = build_tree(tokenlist);
-			push_(treenode, &cmdlist);
-		}
+			handle_leftbracket(&cmdlist, &tokenlist);
 		else if (is_rightbracket(firsubtok[0]))
 		{
 			free_tokennode(pop_tokenlist_head(tokenlist));
 			break ;
 		}
 		else if (is_logicalop(firsubtok))
-		{
-			if (opslist != NULL)
-			{
-				treenode = pop_treenode_from(&opslist);
-				treenode->right = pop_treenode_from(&cmdlist);
-				treenode->left = pop_treenode_from(&cmdlist);
-				push_(treenode, &cmdlist);
-			}
-			treenode = new_treenode(pop_tokenlist_head(tokenlist));
-			push_(treenode, &opslist);
-		}
+			handle_logicalop(&cmdlist, &opslist, &tokenlist);
 		else
-		{
-			treenode = new_treenode(pop_tokenlist_head(tokenlist));
-			push_(treenode, &cmdlist);
-		}
+			handle_default(&cmdlist, &tokenlist);
 	}
-	if (opslist != NULL)
-	{
-		treenode = pop_treenode_from(&opslist);
-		treenode->right = pop_treenode_from(&cmdlist);
-		treenode->left = pop_treenode_from(&cmdlist);
-		push_(treenode, &cmdlist);
-	}
+	empty_opslist(&cmdlist, &opslist);
 	return (pop_treenode_from(&cmdlist));
 }
